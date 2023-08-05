d337e4b90f9a81b44cd5b3733c6d3c45951515fb
Mikhail Seriukov <mseriukov@gmail.com>
2023-08-05 23:13:05 +0300

    Updated grammar.

--- Sources/picoLib/Parser/Parser.swift
+++ Sources/picoLib/Parser/Parser.swift
@@ -1,13 +1,23 @@
 
 /*
-prog: line* EOF
-line: label? opcode argumentlist?
-label: name ':'
-argumentlist: argument (',' argumentlist)?
-opcode: ...
-argument: prefix? (number | name) (('+' | '-') number)? | "(" argument ")"
-prefix: '#' | '='
+ program: line* EOF
 
+ line: (label? instruction? NEWLINE) *
+ instruction: opcode argumentList
+
+ label: identifier ':'
+
+ argumentlist: argument (',' argumentlist)?
+ argument: prefix? (number | name) (('+' | '-') number)? | "(" argument ")"
+
+ prefix: '#' | '='
+
+ register: r1 | r2 | r3 | r4 | r5 | r6 | r7 | r8 | r9 | r10 | r11 | r12 | r13 | r14 | r15 | sp | lr | pc
+ registerList: '{' registerListEntry (',' registerListEntry)? '}'
+ registerListEntry: register | registerRange
+ registerRange: register '-' register
+
+ opcode: ...
 
 */
 
