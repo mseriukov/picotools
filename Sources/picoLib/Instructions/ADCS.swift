ce0029b10de78002aa7d299214fe53a30c7fa2a4
Mikhail Seriukov <mseriukov@gmail.com>
2023-09-11 21:34:49 +0300

    Updated ADCS parsing and error handling.

--- Sources/picoLib/Instructions/ADCS.swift
+++ Sources/picoLib/Instructions/ADCS.swift
@@ -1,20 +1,34 @@
+/// Add with Carry (register) adds a register value, the carry flag value, and an optionally-shifted register value,
+/// and writes the result to the destination register. It updates the condition flags based on the result.
 public struct ADCS: Instruction {
     enum Kind {
-        case ADCS(Register, Register) // ADCS <Rn>, <Rm>
+        case ADCS(Register, Register) // ADCS{<q>} {<Rd>,} <Rn>, <Rm>
     }
     private let kind: Kind
     private let desc: InstructionDescriptor
 
     public init(_ desc: InstructionDescriptor) throws {
         self.desc = desc
-        guard desc.mnemonic == .ADCS else { fatalError("Mnemonic doesn't match the expected one.") }
-        guard desc.condition == nil else { throw InstructionError.unexpectedCondition }
-        guard desc.qualifier == nil else { throw InstructionError.unexpectedQualifier }
+        guard desc.mnemonic == .ADCS else { throw InstructionError.mnemonicMismatch }
+        guard desc.condition == nil || desc.condition == .always else { throw InstructionError.unexpectedCondition }
+        guard desc.qualifier == nil else { throw InstructionError.qualifierIsNotSupported(desc.qualifier!) }
 
-        guard desc.arguments.count == 2 else { throw InstructionError.unexpectedNumberOfArguments }
-        guard case let .register(r1) = desc.arguments[0] else { throw InstructionError.registerExpected(0) }
-        guard case let .register(r2) = desc.arguments[1] else { throw InstructionError.registerExpected(1) }
-        self.kind = .ADCS(r1, r2)
+        if desc.arguments.count == 2 {
+            guard case let .register(r1) = desc.arguments[0] else { throw InstructionError.registerExpected(0) }
+            guard case let .register(r2) = desc.arguments[1] else { throw InstructionError.registerExpected(1) }
+            self.kind = .ADCS(r1, r2)
+            return
+        }
+
+        if desc.arguments.count == 3 {
+            guard case let .register(r1) = desc.arguments[0] else { throw InstructionError.registerExpected(0) }
+            guard case let .register(r2) = desc.arguments[1] else { throw InstructionError.registerExpected(1) }
+            guard case let .register(r3) = desc.arguments[2] else { throw InstructionError.registerExpected(2) }
+            guard r1 == r2 else { throw InstructionError.RdRnMismatch }
+            self.kind = .ADCS(r1, r3)
+            return
+        }
+        throw InstructionError.unexpectedNumberOfArguments
     }
 
     public func encode(symbols: [String: Int]) throws -> [UInt16] {
--- Sources/picoLib/Instructions/InstructionError.swift
+++ Sources/picoLib/Instructions/InstructionError.swift
@@ -3,7 +3,10 @@ import Foundation
 public enum InstructionError: Error {
     case unexpectedCondition
     case unexpectedQualifier
+    case qualifierIsNotSupported(Qualifier)
     case registerExpected(Int)
+    case mnemonicMismatch
+    case RdRnMismatch
     case unexpectedNumberOfArguments
     case undefinedLiteral(String)
     case unknownError
@@ -14,6 +17,9 @@ extension InstructionError: CustomDebugStringConvertible {
         switch self {
         case .unexpectedCondition: return "unexpectedCondition"
         case .unexpectedQualifier: return "unexpectedQualifier"
+        case let .qualifierIsNotSupported(qualifier): return "Qualifier \"\(qualifier)\" is not supported by this instruction."
+        case .mnemonicMismatch: return "Mnemonic doesn't match the expected one."
+        case .RdRnMismatch: return "Rd doesn't match Rn."
         case .unexpectedNumberOfArguments: return "unexpectedNumberOfArguments"
         case let .undefinedLiteral(literal): return "undefinedLiteral \(literal)"
         case let .registerExpected(position): return "register expected at argument position \(position)"
