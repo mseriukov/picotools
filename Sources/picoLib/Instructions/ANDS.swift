caac37ecb7e287694c85267ed4857276da6b99ad
Mikhail Seriukov <mseriukov@gmail.com>
2023-09-13 18:04:56 +0300

    Updated ANDS error handling.

--- Sources/picoLib/Instructions/ANDS.swift
+++ Sources/picoLib/Instructions/ANDS.swift
@@ -1,5 +1,7 @@
 public struct ANDS: Instruction {
     enum Kind {
+        /// This instruction performs a bitwise AND of a register value and an optionally-shifted register value, and writes the result to the destination register.
+        /// It updates the condition flags based on the result.
         case ANDS(Register, Register) // ANDS {<Rd>,} <Rn>, <Rm>
     }
     private let kind: Kind
@@ -11,10 +13,22 @@ public struct ANDS: Instruction {
         guard desc.condition == nil else { throw InstructionError.unexpectedCondition }
         guard desc.qualifier == nil else { throw InstructionError.unexpectedQualifier }
 
-        guard desc.arguments.count == 2 else { throw InstructionError.unexpectedNumberOfArguments }
-        guard case let .register(r1) = desc.arguments[0] else { throw InstructionError.unknownError }
-        guard case let .register(r2) = desc.arguments[1] else { throw InstructionError.unknownError }
-        self.kind = .ANDS(r1, r2)
+        if desc.arguments.count == 2 {
+            guard case let .register(r1) = desc.arguments[0] else { throw InstructionError.registerExpected(0) }
+            guard case let .register(r2) = desc.arguments[1] else { throw InstructionError.registerExpected(1) }
+            self.kind = .ANDS(r1, r2)
+            return
+        }
+
+        if desc.arguments.count == 3 {
+            guard case let .register(r1) = desc.arguments[0] else { throw InstructionError.registerExpected(0) }
+            guard case let .register(r2) = desc.arguments[1] else { throw InstructionError.registerExpected(1) }
+            guard case let .register(r3) = desc.arguments[2] else { throw InstructionError.registerExpected(2) }
+            guard r1 == r2 else { throw InstructionError.RdRnMismatch }
+            self.kind = .ANDS(r1, r3)
+            return
+        }
+        throw InstructionError.unexpectedNumberOfArguments
     }
 
     public func encode(symbols: [String: Int]) throws -> [UInt16] {
