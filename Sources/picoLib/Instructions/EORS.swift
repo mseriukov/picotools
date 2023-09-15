2626807bbce2a8a760af7c59603b42338a15fb68
Mikhail Seriukov <mseriukov@gmail.com>
2023-09-15 19:54:41 +0200

    Updated EORS error handling.

--- Sources/picoLib/Instructions/EORS.swift
+++ Sources/picoLib/Instructions/EORS.swift
@@ -7,14 +7,27 @@ public struct EORS: Instruction {
 
     public init(_ desc: InstructionDescriptor) throws {
         self.desc = desc
-        guard desc.mnemonic == .EORS else { fatalError("Mnemonic doesn't match the expected one.") }
+        guard desc.mnemonic == .EORS else { throw InstructionError.mnemonicMismatch }
         guard desc.condition == nil else { throw InstructionError.unexpectedCondition }
         guard desc.qualifier == nil else { throw InstructionError.unexpectedQualifier }
 
-        guard desc.arguments.count == 2 else { throw InstructionError.unexpectedNumberOfArguments }
-        guard case let .register(r1) = desc.arguments[0] else { throw InstructionError.unknownError }
-        guard case let .register(r2) = desc.arguments[1] else { throw InstructionError.unknownError }
-        self.kind = .EORS(r1, r2)
+        if desc.arguments.count == 2 {
+            guard case let .register(r1) = desc.arguments[0] else { throw InstructionError.registerExpected(0) }
+            guard case let .register(r2) = desc.arguments[1] else { throw InstructionError.registerExpected(1) }
+            self.kind = .EORS(r1, r2)
+            return
+        }
+
+        if desc.arguments.count == 3 {
+            guard case let .register(r1) = desc.arguments[0] else { throw InstructionError.registerExpected(0) }
+            guard case let .register(r2) = desc.arguments[1] else { throw InstructionError.registerExpected(1) }
+            guard case let .register(r3) = desc.arguments[2] else { throw InstructionError.registerExpected(2) }
+            guard r1 == r2 else { throw InstructionError.RdRnMismatch }
+            self.kind = .EORS(r1, r3)
+            return
+        }
+
+        throw InstructionError.unexpectedNumberOfArguments
     }
 
     public func encode(symbols: [String: Int]) throws -> [UInt16] {
