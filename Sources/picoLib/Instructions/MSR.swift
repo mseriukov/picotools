f6049c98716e29c5e1693bdc18408c9ab8dd24b3
Mikhail Seriukov <mseriukov@gmail.com>
2023-09-21 00:46:32 +0300

    Updated MSR error handling.

--- Sources/picoLib/Instructions/MSR.swift
+++ Sources/picoLib/Instructions/MSR.swift
@@ -7,15 +7,17 @@ public struct MSR: Instruction {
 
     public init(_ desc: InstructionDescriptor) throws {
         self.desc = desc
-        guard desc.mnemonic == .MSR else { fatalError("Mnemonic doesn't match the expected one.") }
+        guard desc.mnemonic == .MSR else { throw InstructionError.mnemonicMismatch }
         guard desc.condition == nil else { throw InstructionError.unexpectedCondition }
         guard desc.qualifier == nil else { throw InstructionError.unexpectedQualifier }
 
-        guard desc.arguments.count == 2 else { throw InstructionError.unexpectedNumberOfArguments }
-        guard case let .specialRegister(r1) = desc.arguments[0] else { throw InstructionError.unknownError }
-        guard case let .register(r2) = desc.arguments[1] else { throw InstructionError.unknownError }
 
-        self.kind = .MSR(r1, r2)
+        if desc.arguments.count == 2 {
+            guard case let .specialRegister(r1) = desc.arguments[0] else { throw InstructionError.specialRegisterExpected(0) }
+            guard case let .register(r2) = desc.arguments[1] else { throw InstructionError.registerExpected(1) }
+            self.kind = .MSR(r1, r2)
+        }
+        throw InstructionError.unexpectedNumberOfArguments
     }
 
     public func encode(symbols: [String: Int]) throws -> [UInt16] {
