2f23736ff85b585ffbdfb19942181108b7ad641b
Mikhail Seriukov <mseriukov@gmail.com>
2023-09-22 20:32:44 +0200

    Updated DMB error handling.

--- Sources/picoLib/Instructions/DMB.swift
+++ Sources/picoLib/Instructions/DMB.swift
@@ -7,12 +7,16 @@ public struct DMB: Instruction {
 
     public init(_ desc: InstructionDescriptor) throws {
         self.desc = desc
-        guard desc.mnemonic == .DMB else { fatalError("Mnemonic doesn't match the expected one.") }
+        guard desc.mnemonic == .DMB else { throw InstructionError.mnemonicMismatch }
         guard desc.condition == nil else { throw InstructionError.unexpectedCondition }
         guard desc.qualifier == nil else { throw InstructionError.unexpectedQualifier }
 
-        guard case let .immediate(opt) = desc.arguments[0] else { throw InstructionError.unknownError }
-        self.kind = .DMB(opt)
+        if desc.arguments.count == 1 {
+            guard case let .immediate(opt) = desc.arguments[0] else { throw InstructionError.immediateExpected(0) }
+            self.kind = .DMB(opt)
+            return
+        }
+        throw InstructionError.unexpectedNumberOfArguments
     }
 
     public func encode(symbols: [String: Int]) throws -> [UInt16] {
