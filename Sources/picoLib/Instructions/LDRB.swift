27e72dfdaac95f478c8efb1adf6e15697fd3cb37
Mikhail Seriukov <mseriukov@gmail.com>
2023-09-25 21:10:37 +0200

    Updated LDRB error handling.

--- Sources/picoLib/Instructions/LDRB.swift
+++ Sources/picoLib/Instructions/LDRB.swift
@@ -8,7 +8,7 @@ public struct LDRB: Instruction {
 
     public init(_ desc: InstructionDescriptor) throws {
         self.desc = desc
-        guard desc.mnemonic == .LDRB else { fatalError("Mnemonic doesn't match the expected one.") }
+        guard desc.mnemonic == .LDRB else { throw InstructionError.mnemonicMismatch }
         guard desc.condition == nil else { throw InstructionError.unexpectedCondition }
         guard desc.qualifier == nil else { throw InstructionError.unexpectedQualifier }
 
@@ -26,7 +26,7 @@ public struct LDRB: Instruction {
             return
         }
 
-        throw InstructionError.unknownError
+        throw InstructionError.unexpectedNumberOfArguments
     }
 
     public func encode(symbols: [String: Int]) throws -> [UInt16] {
