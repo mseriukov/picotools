d144040b18e25c56df9acd16a530b2164a5a5bf4
Mikhail Seriukov <mseriukov@gmail.com>
2023-09-28 20:59:19 +0300

    Updated MOV error handling.

--- Sources/picoLib/Instructions/MOV.swift
+++ Sources/picoLib/Instructions/MOV.swift
@@ -7,14 +7,17 @@ public struct MOV: Instruction {
 
     public init(_ desc: InstructionDescriptor) throws {
         self.desc = desc
-        guard desc.mnemonic == .MOV || desc.mnemonic == .CPY else { fatalError("Mnemonic doesn't match the expected one.") }
+        guard desc.mnemonic == .MOV || desc.mnemonic == .CPY else { throw InstructionError.mnemonicMismatch }
         guard desc.condition == nil else { throw InstructionError.unexpectedCondition }
         guard desc.qualifier == nil else { throw InstructionError.unexpectedQualifier }
 
-        guard desc.arguments.count == 2 else { throw InstructionError.unexpectedNumberOfArguments }
-        guard case let .register(r1) = desc.arguments[0] else { throw InstructionError.unknownError }
-        guard case let .register(r2) = desc.arguments[1] else { throw InstructionError.unknownError }
-        self.kind = .MOV(r1, r2)
+        if desc.arguments.count == 2 {
+            guard case let .register(r1) = desc.arguments[0] else { throw InstructionError.registerExpected(0) }
+            guard case let .register(r2) = desc.arguments[1] else { throw InstructionError.registerExpected(1) }
+            self.kind = .MOV(r1, r2)
+            return
+        }
+        throw InstructionError.unexpectedNumberOfArguments
     }
 
     public func encode(symbols: [String: Int]) throws -> [UInt16] {
